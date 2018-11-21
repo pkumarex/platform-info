ed65ff1251d15ab90e938001bab97120b31b0e30
purvades <purva.deshpande@intel.com>
2018-11-21 14:29:52 -0800

    Fixed code review comments.

--- cmd/platforminfo/main.go
+++ cmd/platforminfo/main.go
@@ -8,133 +8,120 @@ import (
 
 func main() {
 	if len(os.Args[1:]) < 1 {
-		log.Fatal("Error while executing integration test. Usage :  ./lib-go-platform-info platforminfoMethodName")
+		log.Fatal("Error while executing command line utility. Usage :  ./main platforminfoMethodName")
 	}
 	var methodName string = os.Args[1]
+	p := platforminfo.New()
 	switch methodName {
 		case "BiosName" :
 			log.Printf("Bios Name method called")
-			p := platforminfo.New()
 			biosName,err := p.BiosName()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of Bios Name: ", err)
 			}
 			log.Printf("Bios Name:%s\n", biosName)
 
 		case "BiosVersion" :
 			log.Printf("Bios Version method called")
-			p := platforminfo.New()
 			biosVersion,err := p.BiosVersion()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of Bios Version: ", err)
 			}
 			log.Printf("Bios Version:%s\n", biosVersion)
 
 		case "HardwareUUID" :
 			log.Printf("Hardware UUID method called")
-			p := platforminfo.New()
 			hardwareUUID,err := p.HardwareUUID()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of Hardware UUID: ", err)
 			}
 			log.Printf("Hardware UUID:%s\n", hardwareUUID)
 
 		case "OSName" :
 			log.Printf("OS Name method called")
-			p := platforminfo.New()
 			osName,err := p.OSName()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of OS Name: ", err)
 			}
 			log.Printf("OS Name:%s\n", osName)
 
 		case "OSVersion" :
 			log.Printf("OS Version method called")
-			p := platforminfo.New()
 			osVersion,err := p.OSVersion()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of OS Version: ", err)
 			}
 			log.Printf("OS Version:%s\n", osVersion)
 
 		case "ProcessorFlags" :
 			log.Printf("Processor Flags method called")
-			p := platforminfo.New()
 			processorFlags,err := p.ProcessorFlags()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of Processor Flags: ", err)
 			}
 			log.Printf("Processor Flags:%s\n", processorFlags)
 
 		case "ProcessorInfo" :
 			log.Printf("Processor Info method called")
-			p := platforminfo.New()
 			processorInfo,err := p.ProcessorInfo()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of Processor Info: ", err)
 			}
 			log.Printf("Processor Info:%s\n", processorInfo)
 
 		case "VMMName" :
 			log.Printf("VMM Name method called")
-			p := platforminfo.New()
 			vmmName,err := p.VMMName()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of VMM Name: ", err)
 			}
 			log.Printf("VMM Name:%s\n", vmmName)
 
 		case "VMMVersion" :
 			log.Printf("VMM Version method called")
-			p := platforminfo.New()
 			vmmVersion,err := p.VMMVersion()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of VMM Version: ", err)
 			}
 			log.Printf("VMM Version:%s\n", vmmVersion)
 
 		case "TPMVersion" :
 			log.Printf("TPM Version method called")
-			p := platforminfo.New()
 			tpmVersion,err := p.TPMVersion()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of TPM Version: ", err)
 			}
 			log.Printf("TPM Version:%s\n", tpmVersion)
 
 		case "HostName" :
 			log.Printf("Host Name method called")
-			p := platforminfo.New()
 			hostName,err := p.HostName()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of Host Name: ", err)
 			}
 			log.Printf("Host Name:%s\n", hostName)
 
 		case "NoOfSockets" :
 			log.Printf("Number of Sockets method called")
-			p := platforminfo.New()
 			noOfSockets,err := p.NoOfSockets()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of Number of Sockets: ", err)
 			}
 			log.Printf("Number of sockets:%s\n", noOfSockets)
 
 		case "TPMEnabled" :
 			log.Printf("TPM Enabled method called")
-			p := platforminfo.New()
 			tpmEnabled,err := p.TPMEnabled()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of status of TPM: ", err)
 			}
 			log.Printf("TPM Enabled:%s\n", tpmEnabled)
 
 		case "TXTEnabled" :
 			log.Printf("TXT Enabled method called")
-			p := platforminfo.New()
 			txtEnabled,err := p.TXTEnabled()
 			if err != nil {
-				log.Fatal("Error occurred: ", err.Error())
+				log.Fatal("Error during retrieval of status of TXT: ", err)
 			}
 			log.Printf("TXT Enabled:%s\n", txtEnabled)
 
